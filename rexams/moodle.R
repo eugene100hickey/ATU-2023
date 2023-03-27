fd12c064ddbf1ad07235128de1acd6bed8c0d1c6
Eugene Hickey <eugene.hickey@tudublin.ie>
2023-03-27 18:35:15 +0100

    added some rexams

---
+++ rexams/moodle.R
@@ -0,0 +1,6 @@
+library(exams)
+elearn_exam <- c("leaflet.Rmd")
+
+set.seed(2021-01-22)
+exams2moodle(elearn_exam, n = 1, name = "R-exams")
+
---
+++ rexams/rexams2moodle.R
@@ -0,0 +1,5 @@
+library(exams)
+elearn_exam <- c("maeve.Rmd")
+
+set.seed(2021-01-22)
+exams2moodle(elearn_exam, n = 3, name = "R-exams")
