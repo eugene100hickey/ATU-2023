e0253956056d529c5e7da09c72a26ccfbf21d4c0
Eugene Hickey <eugene.hickey@tudublin.ie>
2023-04-02 18:01:39 +0100

    murders arrange

--- rexams/rexams2moodle.R
+++ rexams/rexams2moodle.R
@@ -4,7 +4,7 @@ library(exams)
 #                  "rexams/diamonds.Rmd",
 #                  "rexams/penguins-class.Rmd")
 
-elearn_exam <- c("rexams/week-2/ABA-join.Rmd")
+elearn_exam <- c("rexams/week-2/murders-arrange.Rmd")
 
 set.seed(42)
 exams2moodle(elearn_exam, n = 2, name = "rexams")
