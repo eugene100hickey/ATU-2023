6f5ea1b515cc71d9de8205663fee2f93aa59d1dd
Eugene Hickey <eugene.hickey@tudublin.ie>
2023-04-02 11:59:49 +0100

    finished ABA week 2

--- rexams/rexams2moodle.R
+++ rexams/rexams2moodle.R
@@ -4,7 +4,7 @@ library(exams)
 #                  "rexams/diamonds.Rmd",
 #                  "rexams/penguins-class.Rmd")
 
-elearn_exam <- c("rexams/week-2/US-areas.Rmd")
+elearn_exam <- c("rexams/week-2/ABA-group.Rmd")
 
 set.seed(42)
 exams2moodle(elearn_exam, n = 2, name = "rexams")
